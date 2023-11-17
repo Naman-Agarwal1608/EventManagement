4618393acc8ddcea2398d9560fd67b61f2c96c16
Naman Agarwal <agarwal.naman1608@gmail.com>
2023-11-17 21:55:40 +1000

    Added tables | models to store user, events and venues and added dummy data

--- club/events/admin.py
+++ club/events/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Venue
+from .models import MyClubUser
+from .models import Event
 
-# Register your models here.
+admin.site.register([Venue, MyClubUser, Event])
