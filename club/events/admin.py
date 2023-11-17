4eb460802b77f0d752900dfbdb9c71fe2133c185
Naman Agarwal <agarwal.naman1608@gmail.com>
2023-11-17 23:39:23 +1000

    Modifed models and also the admin view

--- club/events/admin.py
+++ club/events/admin.py
@@ -3,4 +3,31 @@ from .models import Venue
 from .models import MyClubUser
 from .models import Event
 
-admin.site.register([Venue, MyClubUser, Event])
+admin.site.register(MyClubUser)
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    '''Admin View for Venue'''
+
+    list_display = ('name', 'address', 'phone')
+    # list_filter = ('',)
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    search_fields = ('name', 'address')
+    # date_hierarchy = ''
+    ordering = ('name',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    '''Admin View for Event'''
+
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')
+    list_filter = ('event_date', 'venue')
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    ordering = ('event_date',)
