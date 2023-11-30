047739471566559b007ed6748aa560bdbb871c45
Naman Agarwal <agarwal.naman1608@gmail.com>
2023-11-30 16:04:39 +1000

    Added new app 'members' for user login|logout|management

--- club/club/urls.py
+++ club/club/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
 
 admin.site.site_header = "My Club Administartion"
