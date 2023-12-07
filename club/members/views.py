9b2a1a5d95228a065c98ce326ff888c3b3f23017
Naman Agarwal <agarwal.naman1608@gmail.com>
2023-12-07 19:44:31 +1000

    Added more fields to user registeration form

--- club/members/views.py
+++ club/members/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 
 
 def login_user(request):
@@ -29,7 +29,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -39,6 +39,6 @@ def register_user(request):
             messages.success(request, "Registration Successfull !!!")
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
 
     return render(request, 'authenticate/register_user.html', {'form': form})
